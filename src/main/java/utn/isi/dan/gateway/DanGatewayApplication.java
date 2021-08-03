d75b616411feef389f186b68ca6e5ad050d15cd7
CiroRiquelmee <ciroriquelme05@gmail.com>
2021-08-02 23:29:26 -0300

    first commit

---
+++ src/main/java/utn/isi/dan/gateway/DanGatewayApplication.java
@@ -0,0 +1,13 @@
+package utn.isi.dan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanGatewayApplication.class, args);
+	}
+
+}
